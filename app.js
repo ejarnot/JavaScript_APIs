478b2f45653ac34e79fd1fb0fd685e9a8e02ebcb
Mia Jarnot <ejarnot42@outlook.com>
2022-04-05 07:40:50 -0700

    finished exercise

--- app.js
+++ app.js
@@ -2,19 +2,27 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+let image;
+let input;
+
 let button = document.getElementById("button");
 
 button.addEventListener("click", fetchRequest)
 
+image = document.querySelector("img")
+input = document.querySelector("#generate")
+
+const GIPHY_URL = "https://api.giphy.com/v1/gifs/translate"
+const GIPHY_KEY = "0jqBxRw7Aj3jYeVBN4T6VF80r91rWLFY";
+
 function fetchRequest(){
-    let image = document.querySelector("img")
-    let input = document.querySelector("#generate").innerHTML
-    fetch("https://api.giphy.com/v1/gifs/translate?api_key=YOURAPIKEY&s=")
+    let searchTerm = input.value //DTo: data transfer object
+    fetch(`${GIPHY_URL}?api_key=${GIPHY_KEY}&s=${searchTerm}`)
         .then((result) =>{
             return result.json();
         })
         .then((data) => {
-            console.log(data);
+            image.src = data.data.images.original.url;
         })
         .catch((err) => {
             console.error(err);

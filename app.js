1e4124dbe93319116023877c685eeef4dc0b7a9d
Mia Jarnot <ejarnot42@outlook.com>
2022-04-04 17:41:59 -0700

    Started the Excercise

--- app.js
+++ app.js
@@ -2,3 +2,22 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+let button = document.getElementById("button");
+
+button.addEventListener("click", fetchRequest)
+
+function fetchRequest(){
+    let image = document.querySelector("img")
+    let input = document.querySelector("#generate").innerHTML
+    fetch("https://api.giphy.com/v1/gifs/translate?api_key=YOURAPIKEY&s=")
+        .then((result) =>{
+            return result.json();
+        })
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+
+}
